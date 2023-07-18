9baee895e1fbb9fc06d3b3c3ae1e2a48d8eefd28
reza <reza.kh.adel@gmail.com>
2023-07-18 14:54:53 +0330

    dockerize django app

--- src/product/models.py
+++ src/product/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import RegexValidator,MinValueValidator,MaxValueValidator
 from django.db import models
+from django.utils.text import slugify
 from category.models import Category
 # Create your models here.
 
@@ -9,20 +10,24 @@ product_varient=(
 )
 
 class Product(models.Model):
-    title=models.CharField(max_length=100)
-    title_brief=models.CharField(max_length=20)
+    title_fa=models.CharField(max_length=100,null=True)
+    title_en=models.CharField(max_length=100,null=True)
     description=models.TextField()
-    image=models.ImageField()
-    slug_url=models.SlugField()
+    image=models.ImageField(upload_to = 'products/')
+    slug_url=models.SlugField(max_length=100,unique=True, blank=True)
     product_varient=models.CharField(choices=product_varient,max_length=50,null=True,blank=True)
     category_1=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,related_name='rootcategory')
     category_2=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,related_name='secound_category')
     category_3=models.ForeignKey(Category,null=True,blank=True,on_delete=models.SET_NULL,related_name='third_category')
     category_4=models.ForeignKey(Category,null=True,blank=True,on_delete=models.SET_NULL,related_name='forth_category')
 
+    def save(self, *args, **kwargs):
+        self.slug_url = slugify(self.title_fa)
+        super(Product, self).save(*args, **kwargs)
+
 
     def __str__(self) -> str:
-        return self.title_brief
+        return self.title_fa
 
 class ProductGallery(models.Model):
     image=models.ImageField()
@@ -66,12 +71,14 @@ variant_types=(
 
 class ProductVarient(models.Model):
     product_id=models.ForeignKey(Product,on_delete=models.CASCADE)
-    size_id=models.ForeignKey(Size,on_delete=models.SET_NULL,null=True)
-    color_id=models.ForeignKey(Color,on_delete=models.SET_NULL,null=True)
+    size_id=models.ForeignKey(Size,on_delete=models.SET_NULL,null=True,blank=True)
+    color_id=models.ForeignKey(Color,on_delete=models.SET_NULL,null=True,blank=True)
     weight=models.IntegerField(default=0,help_text="واحد به گرم میباشد")
     dicount_percent=models.IntegerField(default=0,validators=[MinValueValidator(0,'بازه اعداد باید بالاتر صفر باشد '),MaxValueValidator(100,"تخفیف بالاتر از عدد 100 مجاز نیست ")])
     price=models.DecimalField(max_digits=13,decimal_places=2)
+    is_deafult=models.BooleanField(default=False)
+    quantity=models.IntegerField(default=1,help_text="موجودیت")
     
 
     def  __str__(self) -> str:
-        return self.product_id.title_brief
+        return self.product_id.title_fa
