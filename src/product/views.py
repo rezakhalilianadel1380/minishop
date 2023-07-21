3b73afa545017e622ecc4d4eb3c78f5bef320b0d
reza <reza.kh.adel@gmail.com>
2023-07-21 12:31:33 +0330

    add elided page to paginator

--- src/product/views.py
+++ src/product/views.py
@@ -5,12 +5,16 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
-    paginate_by = 30
+    paginate_by = 2
     template_name = "shop-list.html"  
     context_object_name = "product_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        paginator=context['paginator']
+        page_obj=context["page_obj"]
+        page_obj.adjusted_elided_pages = paginator.get_elided_page_range(page_obj.number)
+        context['page_obj']=page_obj
         return context
 
 
